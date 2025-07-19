bb95d653c97be84822049e129b93bb8eebff3572
kthrnpns <kspkatren@gmail.com>
2025-07-19 18:44:50 +0800

    Delete test.php

--- test.php
+++
@@ -1,8 +0,0 @@
-<?php
-require 'includes/config.php';
-try {
-    $db = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME, DB_USER, DB_PASS);
-    echo "Database connection successful!";
-} catch(PDOException $e) {
-    die("Connection failed: " . $e->getMessage());
-}
